a5713995006f9e0e29e8dc0b00bf1d744cade7ab
Paritosh Chaurasia <149140841+ParitoshAnilNeelamChaurasia@users.noreply.github.com>
2025-03-19 04:31:00 +0530

    Create 9_rising temperature.sql

---
+++ 9_rising temperature.sql
@@ -0,0 +1,13 @@
+-- https://leetcode.com/problems/rising-temperature/?envType=study-plan-v2&envId=top-sql-50
+
+# Write your MySQL query statement below
+SELECT
+    w1.id
+FROM 
+    Weather w1
+INNER JOIN 
+    Weather w2
+WHERE 
+    DATEDIFF(w1.recordDate , w2.recordDate) = 1
+AND
+    w1.temperature > w2.temperature
